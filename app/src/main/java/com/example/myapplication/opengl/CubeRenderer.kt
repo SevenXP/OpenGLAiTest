99d33d1c32b1cc210bfba7c68a7a0fbe417afc3b
Roman Irtov <vendettaangel@gmail.com>
2026-03-14 01:01:58 +0300

    Refactored has been done

--- app/src/main/java/com/example/myapplication/opengl/CubeRenderer.kt
+++ app/src/main/java/com/example/myapplication/opengl/CubeRenderer.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.opengl
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.util.Log
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -11,10 +12,10 @@ class CubeRenderer(private val context: Context) : GLSurfaceView.Renderer {
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-
+        val glVersion = "OpenGL ES ${gl!!.glGetString(GL10.GL_VERSION)}"
+        Log.d("Roma", "GL Version: $glVersion")
     }
 }
